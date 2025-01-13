364cfda8a589e90a955ff3ffb5f787d050148a8c
Omotosho Kamalideen <dapious99@gmail.com>
2025-01-13 09:28:23 +0100

    added and edit on some folders

---
+++ src/app/services/visa-services/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const VisaServices = () => {
+  return <div>page</div>;
+};
+
+export default VisaServices;
--- src/app/studydestinations/study-in-russia/page.tsx
+++ src/app/studydestinations/study-in-russia/page.tsx
@@ -1,9 +1,14 @@
 "use client";
 
 import React from "react";
+import Layout from "@/components/Layout";
 
 const StudyInRussia = () => {
-  return <div>page</div>;
+  return (
+    <Layout>
+      <div>page</div>
+    </Layout>
+  );
 };
 
 export default StudyInRussia;
