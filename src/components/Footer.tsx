b55fcf04cfe179ecacf95f32db7993f7af8a8bcd
Omotosho Kamalideen <dapious99@gmail.com>
2025-01-14 23:57:12 +0100

    added mini layout to footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,6 +4,11 @@ import React from "react";
 const CustomFooter = () => {
   return (
     <footer className="">
+      <div className="flex flex-wrap gap-4">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
       <div className="text-center">
         <p>
           Dansam Travels and Educonsult &#169; {currentYear} || All Rights
