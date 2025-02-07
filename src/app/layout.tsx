08ebe84a04001ac1e9a54a9a53f141127b4ada6a
Kodewarlock <dapious99@gmail.com>
2025-02-07 08:10:04 +0100

    added the keyword metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ export const metadata: Metadata = {
   title: "Dansam Travels and Educonsult",
   description:
     "Dansam travels and educonsult is an agency created specifically for student need to assist them in studying abroad with ease and consulting with educational expert.",
+  keywords:
+    "study abroad, education consultancy, study visa, international education, study in Russia, study in China, study in Belarus,travels,Dansam, Dansam travels and educonsult",
 };
 
 export default function RootLayout({
